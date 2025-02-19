cd9f41a0d7a97d004642117a929033ca39b99829
manapixels <chrenelias@outlook.com>
2025-02-19 14:10:20 +0800

    Add 'use client'

--- src/app/recipes/my/_components/RecipeListInMyRecipes.tsx
+++ src/app/recipes/my/_components/RecipeListInMyRecipes.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 import { useUser } from '@/_contexts/UserContext';
 import RecipeListItemInMyRecipes from './RecipeListItemInMyRecipes';
